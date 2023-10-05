0b50e0467b3c9429025fc908def62b4a23b1cfd8
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-05 22:05:44 +0900

    Update

--- src/dxlib_wrapper/dx_font.rs
+++ src/dxlib_wrapper/dx_font.rs
@@ -1,4 +1,5 @@
-use crate::dxlib_wrapper::dxlib_ffi::dxlib::ToEncode;
+use dxlib::*;
+use dxlib_ffi::*;
 use std::mem::size_of;
 use std::ops::Drop;
 use winapi::um::wingdi::{AddFontResourceExA, RemoveFontResourceExA, FR_PRIVATE};
@@ -22,36 +23,47 @@ pub struct DxFont {
     data: DxFontData,
 }
 impl DxFont {
-    pub fn new(path: &str) -> DxFont {
+    pub fn new() -> DxFont {
         return DxFont {
             data: DxFontData {
-                font_path: path.to_string(),
+                font_path: String::new(),
                 size: 0,
                 thick: 0,
             },
         };
     }
-    pub fn add_resouce_data(&mut self) -> Result<(), String> {
+    pub fn add_resouce_data(&mut self, path: &str) -> Result<(), String> {
         let result = unsafe {
-            AddFontResourceExA(
-                self.data.font_path.to_cstring().as_ptr(),
-                FR_PRIVATE,
-                std::ptr::null_mut(),
-            )
+            AddFontResourceExA(path.to_cstring().as_ptr(), FR_PRIVATE, std::ptr::null_mut())
         };
         if result == 0 {
             return Err("フォントリソースの追加に失敗しました".to_string());
         }
         return Ok(());
     }
-    pub fn delete_resouce_data(&self) -> Result<(), String> {
+    pub fn create_font(
+        &mut self,
+        path: &str,
+        size: i32,
+        thick: i32,
+        font_type: i32,
+    ) -> Result<i32, String> {
+        unsafe {
+            let res = self.add_resouce_data(path);
+            match res {
+                Ok(_) => {}
+                Err(val) => {
+                    return Err(val);
+                }
+            }
+            let handle = dx_CreateFontToHandle(path, size, thick, font_type);
+            return Ok(handle);
+        }
+    }
+    pub fn delete_resouce_data(&self, path: &str) -> Result<(), String> {
         // フォントリソースを削除する
         let result = unsafe {
-            RemoveFontResourceExA(
-                self.data.font_path.to_cstring().as_ptr(),
-                FR_PRIVATE,
-                std::ptr::null_mut(),
-            )
+            RemoveFontResourceExA(path.to_cstring().as_ptr(), FR_PRIVATE, std::ptr::null_mut())
         };
         if result == 0 {
             return Err("フォントリソースの削除に失敗しました".to_string());
@@ -61,7 +73,7 @@ impl DxFont {
 }
 impl Drop for DxFont {
     fn drop(&mut self) {
-       /* let res = self.delete_resouce_data();
+        /* let res = self.delete_resouce_data();
         match res {
             Ok(val) => {
                 println!("Ok({:?})", val);
