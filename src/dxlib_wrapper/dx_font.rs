1c58fefb152c0d8464610c5f7d838fd248be3b15
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-05 20:59:24 +0900

    Update

--- src/dxlib_wrapper/dx_font.rs
+++ src/dxlib_wrapper/dx_font.rs
@@ -1,4 +1,8 @@
-pub struct DxFontData {}
+use std::mem::size_of;
+use winapi::um::wingdi::{AddFontResourceExA, RemoveFontResourceExA, FR_PRIVATE};
+pub struct DxFontData {
+    font_path:String,
+}
 impl DxFontData {
     fn new() -> DxFontData {
         return DxFontData {};
