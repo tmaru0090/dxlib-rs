9e757a994f50c4978600388ebf1f0b715dc116b4
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-05 19:28:07 +0900

    Update

--- src/dxlib_wrapper/dx_keyboard.rs
+++ src/dxlib_wrapper/dx_keyboard.rs
@@ -3,16 +3,16 @@ use crate::dx_common::dxlib::*;
 pub struct DxKeyBoard {
     key_state: [i32; 256],
     key_buf: [i8; 256],
-    press_cnt: i32,
-    release_cnt: i32,
+    press_cnt: [i32;256],
+    release_cnt:[i32;256],
 }
 impl DxKeyBoard {
     pub fn new() -> Box<DxKeyBoard> {
         return Box::new(DxKeyBoard {
             key_state: [0; 256],
             key_buf: [0; 256],
-            press_cnt: 0,
-            release_cnt: 0,
+            press_cnt: [0;256],
+            release_cnt: [0;256],
         });
     }
     pub fn update(&mut self) {
@@ -21,27 +21,25 @@ impl DxKeyBoard {
             dx_GetHitKeyStateAll(self.key_buf.as_mut_ptr());
             // キーの状態を更新
             for i in 0..256 {
-                if self.key_buf[i] == 1 {
-                    self.key_state[i] += 1;
-                    self.press_cnt += 1;
-                    if self.release_cnt > 0 {
-                        self.release_cnt = 0;
+                //self.key_state[i] += 1;
+                if self.key_buf[i] != 0 {
+                    self.press_cnt[i] += 1;
+                    if self.release_cnt[i] > 0 {
+                        self.release_cnt[i] = 0;
                     }
                 } else {
-                    self.key_state[i] = 0;
-                    self.release_cnt += 1;
-                    if self.press_cnt > 0 {
-                        self.press_cnt = 0;
+                    self.release_cnt[i] += 1;
+                    if self.press_cnt[i] > 0 {
+                        self.press_cnt[i] = 0;
                     }
                 }
             }
         }
     }
-    pub fn get_key_state(&self, key_code: i32) -> (i32, i32, i32) {
-        return (
-            self.key_state[key_code as usize],
-            self.press_cnt,
-            self.release_cnt,
-        );
+    pub fn get_press_cnt(&self,key_code:i32)->i32{
+        return self.press_cnt[key_code as usize];
+    }
+    pub fn get_key_state(&self, key_code: i32) -> i32 {
+        return self.key_state[key_code as usize];
     }
 }
