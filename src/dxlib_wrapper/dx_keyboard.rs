eb4f6bd450e051a24f255c8252c1309f2d642a32
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-05 19:37:25 +0900

    Update

--- src/dxlib_wrapper/dx_keyboard.rs
+++ src/dxlib_wrapper/dx_keyboard.rs
@@ -3,16 +3,16 @@ use crate::dx_common::dxlib::*;
 pub struct DxKeyBoard {
     key_state: [i32; 256],
     key_buf: [i8; 256],
-    press_cnt: [i32;256],
-    release_cnt:[i32;256],
+    press_cnt: [i32; 256],
+    release_cnt: [i32; 256],
 }
 impl DxKeyBoard {
     pub fn new() -> Box<DxKeyBoard> {
         return Box::new(DxKeyBoard {
             key_state: [0; 256],
             key_buf: [0; 256],
-            press_cnt: [0;256],
-            release_cnt: [0;256],
+            press_cnt: [0; 256],
+            release_cnt: [0; 256],
         });
     }
     pub fn update(&mut self) {
@@ -36,7 +36,22 @@ impl DxKeyBoard {
             }
         }
     }
-    pub fn get_press_cnt(&self,key_code:i32)->i32{
+    pub fn is_available_code(&self, key_code: i32) -> bool {
+        if 0 >= key_code && key_code > 256 {
+            return false;
+        }
+        return true;
+    }
+    pub fn get_release_cnt(&self, key_code: i32) -> i32 {
+        if !self.is_available_code(key_code) {
+            return -1;
+        }
+        return self.release_cnt[key_code as usize];
+    }
+    pub fn get_press_cnt(&self, key_code: i32) -> i32 {
+        if !self.is_available_code(key_code) {
+            return -1;
+        }
         return self.press_cnt[key_code as usize];
     }
     pub fn get_key_state(&self, key_code: i32) -> i32 {
