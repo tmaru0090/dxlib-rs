0262777f3f852005eb62801d55d3bbc2b2d48f1f
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-05 19:08:25 +0900

    Update

--- src/dxlib_wrapper/dx_keyboard.rs
+++ src/dxlib_wrapper/dx_keyboard.rs
@@ -1,15 +1,18 @@
-
 use crate::dx_common::dxlib::*;
 #[derive(Clone)]
 pub struct DxKeyBoard {
     key_state: [i32; 256],
     key_buf: [i8; 256],
+    press_cnt: i32,
+    release_cnt: i32,
 }
 impl DxKeyBoard {
     pub fn new() -> Box<DxKeyBoard> {
         return Box::new(DxKeyBoard {
             key_state: [0; 256],
             key_buf: [0; 256],
+            press_cnt: 0,
+            release_cnt: 0,
         });
     }
     pub fn update(&mut self) {
@@ -20,13 +23,21 @@ impl DxKeyBoard {
             for i in 0..256 {
                 if self.key_buf[i] == 1 {
                     self.key_state[i] += 1;
+                    self.press_cnt += 1;
+                    self.release_cnt -=1;
                 } else {
                     self.key_state[i] = 0;
+                    self.release_cnt += 1;
+                    self.press_cnt -= 1;
                 }
             }
         }
     }
-    pub fn get_key_state(&self, key_code: i32) -> i32 {
-        return self.key_state[key_code as usize];
+    pub fn get_key_state(&self, key_code: i32) -> (i32, i32, i32) {
+        return (
+            self.key_state[key_code as usize],
+            self.press_cnt,
+            self.release_cnt,
+        );
     }
 }
